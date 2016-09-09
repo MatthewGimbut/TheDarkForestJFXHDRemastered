1405b6205945a6f58a8902f20a791c05de11d45a
MatthewGimbut <mattgimbut@gmail.com>
2016-09-09 19:51:49 -0400

    Small LootPane change
    Converted LootPane UI to fxml and added corresponding controller

--- src/gui/LootPane.java
+++ src/gui/LootPane.java
@@ -2,34 +2,55 @@ package gui;
 
 import characters.Player;
 import items.Item;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import sprites.Lootable;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class LootPane extends BorderPane {
 
-    private GameLabel title;
     private GamePane currentView;
     private Lootable lootContainer;
     private Player player;
-    private GameButton takeAll, exit;
-    private HBox itemBox;
-    private HBox hb;
-
+    @FXML private ResourceBundle resources;
+    @FXML private URL location;
+    @FXML private Button exit;
+    @FXML private FlowPane itemPane;
+    @FXML private Label numItems;
+    @FXML private Button takeAll;
+    @FXML private AnchorPane anchor;
 
     public LootPane(GamePane currentView, Lootable lootContainer, Player player) {
         this.currentView = currentView;
         this.lootContainer = lootContainer;
         this.player = player;
-        itemBox = new HBox(15);
-        this.setId("standardPane");
-        this.title = new GameLabel(lootContainer.getItems().size() + (lootContainer.getItems().size() == 1 ? " item" : " items"));
 
-        takeAll = new GameButton("Take All");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource(
+                "gui\\LootPane.fxml"));
+        fxmlLoader.setController(this);
+
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    @FXML
+    void initialize() {
+
+        exit.setOnAction(event -> currentView.removeLootPane(this));
+
         takeAll.setOnAction(event -> {
             int totalWeight = 0;
 
@@ -38,39 +59,19 @@ public class LootPane extends BorderPane {
             }
 
             if((player.getCarryCap() - player.getCurrentCarry() > totalWeight)) {
-                lootContainer.getItems().forEach(i -> player.addItem(i));
+                lootContainer.getItems().forEach(player::addItem);
                 lootContainer.getItems().removeAll(lootContainer.getItems());
                 currentView.removeLootPane(this);
             } else {
-                title.setText("Not enough space!");
+                numItems.setText("Not enough space!");
             }
         });
 
-        exit = new GameButton("Exit");
-        exit.setOnAction(event -> {
-            currentView.removeLootPane(this);
-        });
-
-        hb = new HBox(5);
-        hb.setAlignment(Pos.CENTER);
-        hb.getChildren().addAll(takeAll, exit);
-
-        HBox titleWrapper = new HBox();
-        titleWrapper.setAlignment(Pos.CENTER);
-        titleWrapper.getChildren().add(title);
-
         drawItems();
-        checkIfEmpty();
-        this.setTop(titleWrapper);
-        this.setBottom(hb);
-        this.setCenter(itemBox);
-        hb.setMargin(itemBox, new Insets(5,0,0,0));
-        if(lootContainer.getItems().size() > 0) {
-            this.setMaxWidth(100 + 80*lootContainer.getItems().size());
-        } else {
-            this.setMaxWidth(150);
-        }
-        this.setMaxHeight(120);
+
+        numItems.setText(lootContainer.getItems().size() + " Items");
+
+        this.setCenter(anchor);
     }
 
     private void drawItems() {
@@ -78,32 +79,19 @@ public class LootPane extends BorderPane {
             lootContainer.getItems().forEach(i -> {
                 VBox vb = new VBox();
                 vb.getChildren().add(new ItemPane(i, player));
-                GameButton gb = new GameButton("Take");
+                Button gb = new Button("Take");
                 gb.setOnAction(event -> {
                     if(!player.addItem(i)) {
                         currentView.displayMessagePane("You cannot carry that!");
                     } else {
                         lootContainer.getItems().remove(i);
                     }
-                    itemBox.getChildren().remove(vb);
-                    checkIfEmpty();
+                    itemPane.getChildren().remove(vb);
                 });
                 vb.setAlignment(Pos.CENTER);
                 vb.getChildren().add(gb);
-                itemBox.getChildren().add(vb);
+                itemPane.getChildren().add(vb);
             });
-        }
-        this.setMargin(itemBox, new Insets(5, 0, 5, 0));
-        itemBox.setAlignment(Pos.CENTER);
-    }
-
-    private void checkIfEmpty() {
-        if(lootContainer.getItems().size() == 0) {
-            GameLabel GameLabel = new GameLabel("Empty");
-            //GameLabel.setFont(new Font("Cambria", 18));
-            this.setAlignment(GameLabel, Pos.CENTER);
-            this.setTop(GameLabel);
-            takeAll.setDisable(true);
         }
     }
 }
