c5d0aaf2717001e46d1de981b004e54bc081edfc
szimmy <seanwzimmerman@gmail.com>
2016-09-27 20:42:17 -0400

    Added BattleHandler (no functionality)
    Minor changes to GamePane too, StatChange is now depricated

--- src/attacks/Other/StatChange.java
+++ src/attacks/Other/StatChange.java
@@ -6,6 +6,7 @@ import java.util.Map;
  * @author Sean Zimmerman
  *
  */
+@Deprecated
 public abstract class StatChange {
 
 	private int numTurns;
---
+++ src/battle/BattleHandler.java
@@ -0,0 +1,18 @@
+package battle;
+
+import characters.*;
+import characters.Character;
+
+/**
+ * battle.BattleHandler is the controller for the Battle System.
+ * Handles attacks between players and enemies, and deals with death.
+ * All methods are static.
+ *
+ * @author Sean Zimmerman
+ */
+public class BattleHandler {
+
+    public static void attack(Character attacker, Character defender) {
+
+    }
+}
